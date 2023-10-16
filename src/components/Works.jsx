67a52220eafbf90cbe6ec9b4ff60ed096f8de76f
Dima <dimon.belenok@gmail.com>
2023-10-16 17:13:05 +0300

    end

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -21,7 +21,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl w-full md:w-[360px] '
+        className='bg-tertiary p-5 rounded-2xl md:w-[360px] w-full '
       >
         <div className='relative w-full h-[230px]'>
           <img

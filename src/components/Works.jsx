8fd23dc04aa5fd71cfb980a80a2d9778fe5d047e
Dima <dimon.belenok@gmail.com>
2023-10-16 18:29:35 +0300

    delete features))

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,7 +14,7 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", 1.2 * 0.5, 0.75)}>
+
       <Tilt
         options={{
           max: 45,
@@ -60,20 +60,19 @@ const ProjectCard = ({
           ))}
         </div>
       </Tilt>
-    </motion.div>
+    
   );
 };
 
 const Works = () => {
   return (
     <React.Fragment >
-      <motion.div id='projects' variants={textVariant()}>
+      <div id='projects'>
         <h2 className='text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>Projects.</h2>
-      </motion.div>
+      </div>
 
       <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+        <p
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through
@@ -81,12 +80,12 @@ const Works = () => {
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        </motion.p>
+        </p>
       </div>
 
       <div className='mt-20 flex flex-wrap gap-7'>
         <ProjectCard 
-          tags={[{name : 'react'},{name : 'zutand'},]}
+          tags={[{name : 'react'},{name : 'zustand'},]}
           name={'Beer App'}
           image={beer}
           source_code_link={'https://beer-recipes-punk-api.netlify.app/'}
